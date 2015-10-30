5fec66103f68f4ff2f260f0d1d33c42952cd77fc
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2015-10-30 10:11:37 -0300

    Problema da mochila para resolução da questão do SPOJ.

--- DynamicProgramming/ProblemaMochila.java
+++ DynamicProgramming/ProblemaMochila.java
@@ -2,69 +2,67 @@ package exemplos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ProblemaMochila {
 
-	public static Integer[][] inicializaMatriz(Integer [][] array) {
+	public static Integer[][] inicializaMatriz(Integer[][] array) {
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array[0].length; j++) {
-				if (i == 0 || j == 0){
+				if (i == 0 || j == 0) {
 					array[i][j] = 0;
 				}
 			}
 		}
 		return array;
 	}
-	
-	public static Integer[][] MochilaProgramacaoDinamica(Integer[]v, Integer[]w, Integer W){
-		Integer[][] V = new Integer[v.length+1][W+1];
+
+	public static Integer[][] MochilaProgramacaoDinamica(Integer[] v, Integer[] w, Integer W) {
+		Integer[][] V = new Integer[v.length + 1][W + 1];
 		V = inicializaMatriz(V);
-		
+
 		for (int i = 1; i <= v.length; i++) {
 			for (int j = 1; j <= W; j++) {
-				if(w[i-1] > j){
-					V[i][j] = V[i-1][j];
+				if (w[i - 1] > j) {
+					V[i][j] = V[i - 1][j];
+				} else {
+					V[i][j] = max(V[i - 1][j], V[i - 1][j - w[i - 1]]
+							+ v[i - 1]);
 				}
-				else{
-					V[i][j] = max(V[i-1][j], V[i-1][j-w[i-1]] + v[i-1]);
-				}
-			}		
+			}
 		}
 		return V;
 	}
-	
-	public static List<Integer> Reconstrucao(Integer[]v, Integer[]w, Integer W, Integer[][] V){
-		List<Integer> S = new ArrayList<Integer>();
-		int i = v.length;
-		int j = W;
-		
-		while(i >= 1){
-			if (w[i-1] <= j && V[i-1][j] < V[i-1][j-w[i-1]] + v[i-1]){
-				S.add(i);
-				j = j-w[i-1];
-			}
-			i = i-1;
-		}
-		return S;
-	}
-	
-	
+
 	private static int max(int x, int y) {
-		if (x >= y){
+		if (x >= y) {
 			return x;
 		}
 		return y;
 	}
-	
+
 	public static void main(String[] args) {
-		Integer[]v = {12,10,20,15};
-		Integer[]w = {2,1,3,2}; 
-		Integer W = 5;
-		
-		Integer[][] V = MochilaProgramacaoDinamica(v, w, W);
-		Integer result = V[v.length][W];
-		
+		Scanner sc = new Scanner(System.in);
+		String aux = sc.nextLine();
+
+		String[] aux2 = aux.split(" ");
+
+		Integer S = Integer.valueOf(aux2[0]);
+		Integer N = Integer.valueOf(aux2[1]);
+
+		Integer[] v = new Integer[N];
+		Integer[] w = new Integer[N];
+
+		for (int i = 0; i < N; i++) {
+			aux = sc.nextLine();
+			aux2 = aux.split(" ");
+			w[i] = Integer.valueOf(aux2[0]);
+			v[i] = Integer.valueOf(aux2[1]);
+		}
+
+		Integer[][] V = MochilaProgramacaoDinamica(v, w, S);
+		Integer result = V[v.length][S];
+
 		System.out.println(result);
-		System.out.println(Reconstrucao(v, w, W, V));
 	}
 }
